9224ffd4285801f018bce4b3f9d714a8dd865c2b
vividswan <vividswan@naver.com>
2024-02-21 01:19:44 +0900

    project init

---
+++ src/main/java/com/vividswan/practice/AssignmentPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.vividswan.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vividswan/practice/AssignmentPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vividswan.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
